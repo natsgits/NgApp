ff0196b16e7dced6115a6144e6b0fde21954fd5e
Natesan <natesh2311@gmail.com>
2024-09-10 14:25:19 +0530

    composing components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalcComponent } from './calc/calc.component';
+import { InfoComponent } from './info/info.component';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CalcComponent,InfoComponent,FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
